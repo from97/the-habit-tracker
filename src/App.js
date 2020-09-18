4b9db10e9c512b92d2ac7a0107daf365fd5b7af0
from97 <skm1104@daum.net>
2020-09-18 19:31:06 +0900

    make Subject component and import in App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    this.state = {
+      subject: { title: 'The Habit Traker', sub: 'make good habits'}
+    }
+  }
   render () {
     return (
       <div className="App">
+        <Subject title={this.state.subject.title}></Subject>
       </div>
     );
   }
