8282faaada794fedfc4b5ccfaea4a7b319aa655d
from97 <skm1104@daum.net>
2020-09-23 22:37:35 +0900

    Updates

---
+++ precache-manifest.d6097a334a545e2ebb3de86c775d8c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94bb34dd5ded771477befd576ca2f74",
+    "url": "/the-habit-tracker/index.html"
+  },
+  {
+    "revision": "97d9d5dd7e559799eb92",
+    "url": "/the-habit-tracker/static/css/main.1054f6e5.chunk.css"
+  },
+  {
+    "revision": "ad94a88ec4fff7170a21",
+    "url": "/the-habit-tracker/static/js/2.9ef09162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-habit-tracker/static/js/2.9ef09162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d9d5dd7e559799eb92",
+    "url": "/the-habit-tracker/static/js/main.efb7d546.chunk.js"
+  },
+  {
+    "revision": "09031ca51645ef2923ae",
+    "url": "/the-habit-tracker/static/js/runtime-main.5648abc0.js"
+  }
+]);
