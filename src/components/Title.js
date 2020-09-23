0224e7ccbb8dae0500739783cdb3917bf56a37b4
from97 <skm1104@daum.net>
2020-09-23 20:57:08 +0900

    icon 배경 추가

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ class Title extends Component {
     render() {
       return (
         <header>
-          <i class="far fa-calendar-check"></i>
+          <i class="far fa-calendar-check fa-2x"></i>
           <h1>{this.props.title}</h1>
           <p>Just another free, fully responsive site template<br></br>
 built by @ajlkn for HTML5 UP.</p>
