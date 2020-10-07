05659b59a48052b96f211af87c9f2c52e0b3b378
from97 <skm1104@daum.net>
2020-10-07 21:16:51 +0900

    add HomeContent Component

---
+++ src/components/HomeContent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HomeContent extends Component {
+    render() {
+      return (
+        <article>content</article>
+      );
+    }
+}
+
+export default HomeContent;
