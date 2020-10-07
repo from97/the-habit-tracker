468ca294608adb1536860bafe1419cb9ce506735
from97 <skm1104@daum.net>
2020-10-07 22:05:14 +0900

    HabitList props에서 data가져와서 렌더

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 class HabitList extends Component {
     render() {
         var list = [];
+        var habits = this.props.data;
+        var i = 0;
+        while (i < habits.length) {
+            list.push(<li key={habits[i]}>{habits[i]}</li>);
+            i++;
+        }
         return (
             <ul>
                 {list}
