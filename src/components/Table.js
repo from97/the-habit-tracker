0c6074f3c3968db4be7227e5ed9160a53302046e
from97 <skm1104@daum.net>
2020-10-08 01:18:17 +0900

    table 헤더로 dateList 추가, 테이블 완성

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 class Table extends Component {
     render() {
         var habitList = [];
+        var dateList = [];
         var habits = this.props.data;
+        var DAYS_IN_MONTH = 31;
+
         var i = 0;
         while (i < habits.length) {
             habitList.push(<tr key={habits[i]}>{habits[i]}</tr>);
             i++;
         }
+
+        i = 1;
+        while (i <= DAYS_IN_MONTH) {
+            dateList.push(<th>{i}</th>);
+            i++;
+        }
         return (
             <table>
+                <th></th>
+                {dateList}
                 {habitList}
             </table>
         );
