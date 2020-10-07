1061e260c949b5fc16523af9903bf668cb51d251
from97 <skm1104@daum.net>
2020-10-08 01:10:08 +0900

    Table에 tr(table-row)로 habit list 추가

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class Table extends Component {
     render() {
+        var habitList = [];
+        var habits = this.props.data;
+        var i = 0;
+        while (i < habits.length) {
+            habitList.push(<tr key={habits[i]}>{habits[i]}</tr>);
+            i++;
+        }
         return (
             <table>
-
+                {habitList}
             </table>
         );
     }
