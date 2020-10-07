df59f9f70bf40abac2894c62d6b7870c14541874
from97 <skm1104@daum.net>
2020-10-08 02:47:35 +0900

    table 구조 & 스타일 변경

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,25 +3,28 @@ import React, { Component } from 'react';
 class Table extends Component {
     render() {
         var habitList = [];
+        var tdList = [];
         var dateList = [];
         var habits = this.props.data;
         var DAYS_IN_MONTH = 31;
 
-        var i = 0;
-        while (i < habits.length) {
-            habitList.push(<tr key={habits[i]}>{habits[i]}</tr>);
+        var i = 1;
+        while (i <= DAYS_IN_MONTH) {
+            tdList.push(<td><input type='checkbox'></input></td>);
+            dateList.push(<td>{i}</td>);
             i++;
         }
 
-        i = 1;
-        while (i <= DAYS_IN_MONTH) {
-            dateList.push(<th>{i}</th>);
+        i = 0;
+        while (i < habits.length) {
+            habitList.push(<tr key={habits[i]}>{habits[i]}{tdList}</tr>);
             i++;
         }
+
+        
         return (
             <table>
-                <th></th>
-                {dateList}
+                <tr>Habits{dateList}</tr>
                 {habitList}
             </table>
         );
