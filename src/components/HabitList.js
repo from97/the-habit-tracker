9ad28fc70b585ad21fc87fc84fa60a6ef6ee80d2
from97 <skm1104@daum.net>
2020-10-07 21:49:15 +0900

    HabitList 컴포넌트 추가

---
+++ src/components/HabitList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HabitList extends Component {
+    render() {
+        var list = [];
+        return (
+            <ul>
+                {list}
+            </ul>
+        );
+    }
+}
+
+export default HabitList;
